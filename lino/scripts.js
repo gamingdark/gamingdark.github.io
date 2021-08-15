0f100eb49235432877fef7c64ea1807425ec985d
gamingdark <excgames@gmail.com>
2021-08-15 19:01:50 +0300

    Add files via upload

--- lino/scripts.js
+++ lino/scripts.js
@@ -1,31 +1,26 @@
 $(function() {
-	generateImages('images/covers/', '#covers-gallery');
-	generateImages('images/articles/', '#articles-gallery');
-    
-    lightGallery(document.getElementById('covers-gallery'), {
-        plugins: [lgZoom, lgThumbnail],
-        speed: 500,
-		licenseKey: 'lino',
-		thumbnails: true
-    });
-    
-    lightGallery(document.getElementById('articles-gallery'), {
-        plugins: [lgZoom, lgThumbnail],
-        speed: 500,
-		licenseKey: 'lino',
-		thumbnails: true
-    });
+	generateImages('images/covers/', 'covers-gallery');
+	generateImages('images/articles/', 'articles-gallery');
 });
 
 function generateImages(folderUrl, idtag) {
-	console.log($(idtag));
     $.get(folderUrl+'list.txt', function(data) {
 		var list = data.replaceAll('\r','').split('\n');
-		console.log(list);
 		for (i in list) {
 			var imgTag = '<a href="'+folderUrl+'/'+list[i]+'"><img src="'+folderUrl+'/thumbnails/'+list[i]+'" /></a>';
-			$(idtag).append(imgTag);
+			$('#'+idtag).append(imgTag);
 		}
+		
+		initGallery(idTag);
     }); 
+}
+
+function initGalery(idTag) {
+    lightGallery(document.getElementById(idTag), {
+        plugins: [lgZoom, lgThumbnail],
+        speed: 500,
+		licenseKey: 'lino',
+		thumbnails: true
+    });
 }
     
