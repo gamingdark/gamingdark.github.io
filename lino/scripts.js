62fac5264b7df9d9d1ad45de7ad67127c65e2a17
gamingdark <excgames@gmail.com>
2021-08-15 19:22:12 +0300

    Update scripts.js

--- lino/scripts.js
+++ lino/scripts.js
@@ -3,12 +3,12 @@ $(function() {
 	generateImages('images/articles/', 'articles-gallery');
 });
 
-function generateImages(folderUrl, idtag) {
+function generateImages(folderUrl, idTag) {
     $.get(folderUrl+'list.txt', function(data) {
 		var list = data.replaceAll('\r','').split('\n');
 		for (i in list) {
 			var imgTag = '<a href="'+folderUrl+'/'+list[i]+'"><img src="'+folderUrl+'/thumbnails/'+list[i]+'" /></a>';
-			$('#'+idtag).append(imgTag);
+			$('#'+idTag).append(imgTag);
 		}
 		
 		initGallery(idTag);
