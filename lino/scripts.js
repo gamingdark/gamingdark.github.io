db7032e66854c9101c1e867d940746342329f9a6
gamingdark <excgames@gmail.com>
2021-08-15 19:04:14 +0300

    Update scripts.js

--- lino/scripts.js
+++ lino/scripts.js
@@ -15,7 +15,7 @@ function generateImages(folderUrl, idtag) {
     }); 
 }
 
-function initGalery(idTag) {
+function initGallery(idTag) {
     lightGallery(document.getElementById(idTag), {
         plugins: [lgZoom, lgThumbnail],
         speed: 500,
